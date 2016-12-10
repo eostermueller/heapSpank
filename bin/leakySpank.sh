f54ab8f798c3b8ea76845e71c946eefaf56bf24b
Erik Ostermueller <eostermueller@gmail.com>
2016-12-10 06:54:40 -0600

    java -jar now launches leakySpank

--- bin/leakySpank.sh
+++
@@ -1,8 +0,0 @@
-dir=$(dirname "$0")
-
-
-export MY_CP=$dir/../target/heapSpank-0.5.jar:$dir/heapSpank-0.5.jar
-
-
-#javap -cp $MY_CP com.github.eostermueller.heapspank.leakyspank.console.LeakySpankConsole
-java -cp $MY_CP com.github.eostermueller.heapspank.leakyspank.console.LeakySpankConsole $1
