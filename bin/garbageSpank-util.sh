e642363303b8cc885ac8201f5ce87b55c42eb966
Erik Ostermueller <eostermueller@gmail.com>
2016-12-10 07:04:47 -0600

    script & version number cleanup

--- bin/garbageSpank-util.sh
+++ bin/garbageSpank-util.sh
@@ -1,10 +1,11 @@
 dir=$(dirname "$0")
 
 
-export GS=$dir/../target/heapSpank-0.3.jar:$dir/heapSpank-0.3.jar
+#One location for a dev environment, one location for binary packaging
+export GS=$dir/../target/heapSpank-0.6.jar:$dir/heapSpank-0.6.jar
 
 export PID_TO_MONITOR=$1
 export INTERVAL=1s
-export OPTION=-gcold
+export OPTION=-gcutil
 
 $JAVA_HOME/bin/jstat $OPTION $PID_TO_MONITOR $INTERVAL | java -classpath $GS com.github.eostermueller.heapspank.garbagespank.console.GarbageSpankConsole -i $INTERVAL
